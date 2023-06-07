0c925d64346c1aa7e5c30250dfb29140762bbdd4
sankar0528 <57930899+sankar0528@users.noreply.github.com>
2023-06-07 23:50:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { SeatReservationComponent } from './seat-reservation/seat-reservation.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeatReservationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
